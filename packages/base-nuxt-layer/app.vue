a4971a31a6b50cd37913e990f27e3e98754e97a8
Hugo <hmoralesg@icloud.com>
2024-09-23 18:47:36 -0300

    feat: added excalidraw board component to base template

--- packages/base-nuxt-layer/app.vue
+++ packages/base-nuxt-layer/app.vue
@@ -1,3 +1,18 @@
 <template>
-  <HelloWorld />
+  <div class="excaliboard">
+    <ExcalidrawBoard />
+  </div>
 </template>
+
+<script setup lang="ts">
+import ExcalidrawBoard from './components/ExcalidrawBoard.vue';
+</script>
+
+<style scoped>
+.excaliboard {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
