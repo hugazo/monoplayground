47f9b245a7c8ec841150496fbea94b5c765db5f5
Hugo <hmoralesg@icloud.com>
2024-10-01 20:37:57 -0300

    feat: adds color mode button

---
+++ packages/base-nuxt-layer/components/ColorModeButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <ClientOnly>
+    <UiButton @click="mode = mode === 'dark' ? 'light' : 'dark'">
+      {{ mode }}
+    </UiButton>
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+const mode = useColorMode();
+</script>
