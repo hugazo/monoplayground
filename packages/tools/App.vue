a7fa07f32bbbd4395e0dee913efa02dcdfdc777f
Hugo <hmoralesg@icloud.com>
2024-10-02 03:29:42 -0300

    feat: nuxt tools layout

---
+++ packages/tools/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>I am the tool module</p>
+</template>
---
+++ packages/tools/nuxt.config.ts
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  devtools: {
+    enabled: true,
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        semi: true,
+      },
+    },
+  },
+  modules: ['@nuxt/eslint'],
+  compatibilityDate: '2024-10-02',
+
+})
