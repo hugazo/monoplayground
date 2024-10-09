b0b908d8b129e2ac5c754239d5517a1bbb026114
Hugo <hmoralesg@icloud.com>
2024-10-09 00:39:51 -0300

    chore: updated references

--- apps/nuxt-app/nuxt.config.ts
+++ apps/nuxt-app/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [
-    '@monoplayground/base-layer',
+    '@monoplayground/ui',
   ],
   devtools: { enabled: true },
   compatibilityDate: '2024-09-24',
