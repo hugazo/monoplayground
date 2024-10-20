584ac318af20e2915046a49f0d09b06a774ad553
Hugo <hmoralesg@icloud.com>
2024-10-19 21:37:39 -0300

    feat: working basic logic to save board

--- apps/whiteboard/pages/boards/[id].vue
+++ apps/whiteboard/pages/boards/[id].vue
@@ -4,12 +4,25 @@
       <ExcalidrawBoard
         :theme="getTheme"
         :on-change="handleOnChange"
-        :initial-data="{ elements: boardData?.data, files: boardData?.files }"
+        :initial-data="initialData"
       />
     </div>
     <div>
+      <UiButton
+        class="w-24"
+        :variant="enableAutoUpdate ? 'default' : 'ghost'"
+        @click="enableAutoUpdate = !enableAutoUpdate"
+      >
+        Auto Update
+      </UiButton>
+      <UiButton
+        class="w-24"
+        :loading="loading"
+        @click="manualSave"
+      >
+        Save
+      </UiButton>
       <p>{{ $route.params.id }}</p>
-      <pre>{{ boardData }}</pre>
     </div>
   </div>
 </template>
@@ -27,13 +40,36 @@ const getTheme = computed(() => {
   return theme.value === 'dark' ? 'dark' : 'light';
 });
 
+const enableAutoUpdate = ref(false);
+const loading = ref(false);
+
+const initialData = computed(() => {
+  if (!boardData?.value?.data) return { elements: [], files: {} };
+  return {
+    elements: boardData.value.data as ExcalidrawElement[],
+    files: boardData.value.files,
+  };
+});
+
+const manualSave = async () => {
+  if (!boardData.value) return;
+  loading.value = true;
+  await logChange(boardData.value.data, {} as AppState, boardData.value.files);
+  loading.value = false;
+};
+
 const logChange = async (
   elements: readonly ExcalidrawElement[],
   _state: AppState,
   files: BinaryFiles,
 ) => {
+  if (!enableAutoUpdate.value) return;
   const data = elements as ExcalidrawElement[];
   await $client.board.update.mutate({ id: route.params.id as string, data, files });
+  if (boardData.value) {
+    boardData.value.data = data;
+    boardData.value.files = files;
+  }
 };
 
 const waitTime = 1000;
